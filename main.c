777660b167498f3f9ec06b913d2e951dd22b2c99
phillipoh102 <phillipkitavi555@gmail.com>
2023-05-24 12:14:56 +0300

    main.c

--- main.c
+++ main.c
@@ -12,11 +12,6 @@ int main(int ac, char **av)
 	info_t info[] = { INFO_INIT };
 	int fd = 2;
 
-	asm ("mov %1, %0\n\t"
-			"add $3, %0"
-			: "=r" (fd)
-			: "r" (fd));
-
 	if (ac == 2)
 	{
 		fd = open(av[1], O_RDONLY);
